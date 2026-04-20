661286efef23ba487fdcb88a581cf58d6076386a
Preetham-Malaga <malagapreetham0111@gmail.com>
2026-04-20 22:24:22 +0530

    Add files via upload

---
+++ Calculator.js
@@ -0,0 +1,5 @@
+function add(){
+    let a="Number"(document.getElementById ("number1").value);
+    let b="Number"(document.getElementById ("number2").value);
+document.getElementById("result").innerText = a+b;
+}
